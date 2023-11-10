82f478420949b41f24dd22fa9d2ee817cc36c7bb
Lucas Vilar <lucas.vilar360@gmail.com>
2023-11-10 20:39:29 -0300

    feat: create form structure and style

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.css'
+})
+export class FormsComponent {
+  results = [];
+}

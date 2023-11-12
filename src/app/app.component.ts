e76a8fc203062cc306d65802b14aab7cbc07736d
Lucas Vilar <lucas.vilar360@gmail.com>
2023-11-12 15:47:04 -0300

    feat: add app-container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
